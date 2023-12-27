1cbb3426d5c91b8728c2d542bffbfcffb50ae2bf
Abdullah Shah <abdullah-shah97@hotmail.com>
2023-12-27 14:31:55 +0000

    Component which lists the items in a shopping list state variable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from "./components/HomePage";
 import UserLogin from "./components/UserLogin";
+import SearchFunctionality from "./components/SearchFunctionality";
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<UserLogin />} />
+                <Route path="/search" element={<SearchFunctionality />} />
             </Routes>
         </Router>
     );
---
+++ src/components/SearchFunctionality.jsx
@@ -0,0 +1,58 @@
+import {useState} from "react";
+
+const SearchFunctionality = () => {
+    const [search, setSearch] = useState('')
+    const [shopping, setShopping] = useState([
+        {
+            name: 'Milk',
+            quantity: 2,
+            price: 1,
+            discount: 0
+        },
+        {
+            name: 'Crisp',
+            quantity: 5,
+            price: 2.5,
+            discount: 10
+        },
+        {
+            name: 'Bread',
+            quantity: 1,
+            price: 0.5,
+            discount: 10
+        },
+        {
+            name: 'Socks',
+            quantity: 4,
+            price: 5,
+            discount: 20
+        }
+    ])
+
+    function handleInputChangeSearch(e) {
+        e.preventDefault()
+        setSearch(e.target.value)
+    }
+
+    function handleSubmitSearch(e) {
+        return undefined;
+    }
+
+    return(
+        <div>
+            <input type = "search" name ="searchInput" onChange={e => handleInputChangeSearch(e)}/>// <button onClick={e => handleSubmitSearch(e)}>Search</button>
+            <ul>
+            {shopping.map((item, index) => (
+                    <li key={index}>
+                        <h3>Name: {item.name}</h3>
+                        <h5>Price: {item.price}</h5>
+                        <h5>Quantity: {item.quantity}</h5>
+                        <h5>Discount: {item.discount}%</h5><br/>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default SearchFunctionality
